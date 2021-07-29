5b3f4a96c7caaed4b61826657a729c4c85039b76
Shak623 <shak.farooq2@gmail.com>
2021-07-28 20:00:47 -0400

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TicketComponent } from './ticket/ticket.component';
+import { TicketsComponent } from './tickets/tickets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tickets',
+    component: TicketsComponent
+  },
+  {
+    path: 'ticket/:id',
+    component: TicketComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
