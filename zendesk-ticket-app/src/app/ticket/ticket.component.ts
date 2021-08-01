a21baf1be8fbd67c0fe5f0f1277459315935fc4b
Shak623 <shak.farooq2@gmail.com>
2021-08-01 12:01:20 -0400

    Implemented prev and next buttons

--- zendesk-ticket-app/src/app/ticket/ticket.component.ts
+++ zendesk-ticket-app/src/app/ticket/ticket.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountResponseModel } from '../models/CountResponseModel';
 import { TicketResponseModel } from '../models/TicketResponseModel';
 import { TicketsService } from '../services/tickets.service';
 
@@ -12,11 +13,13 @@ import { TicketsService } from '../services/tickets.service';
 export class TicketComponent implements OnInit {
 
   ticket!: TicketResponseModel;
+  count!: CountResponseModel;
   message: string = "";
 
   constructor(private ticketsService: TicketsService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
+    this.getCount();
     this.getTicket();
   }
 
@@ -25,6 +28,20 @@ export class TicketComponent implements OnInit {
     this.ticketsService.getTicket(id).subscribe(s => {
       this.ticket = s;
     })
+  }
+
+  getCount(): void {
+    this.ticketsService.getCount().subscribe(s => {
+      this.count = s;
+    })
+  }
+
+  goPrev(id: number): void {
+    window.location.href = `${window.location.protocol}//${window.location.host}/ticket/${id - 1}`;
+  }
+
+  goNext(id: number): void {
+    window.location.href = `${window.location.protocol}//${window.location.host}/ticket/${id + 1}`;
   }
 
 }
