1a5f56b7a796061b689e0c6b0e4640e542d6dd4a
Shak623 <shak.farooq2@gmail.com>
2021-08-02 18:28:59 -0400

    Fixed the rest of the errors

--- zendesk-ticket-app/src/app/tickets/tickets.component.spec.ts
+++ zendesk-ticket-app/src/app/tickets/tickets.component.spec.ts
@@ -1,19 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TicketsService } from '../services/tickets.service';
+import { MockTicketsService } from '../services/tickets.service.mock';
 
 import { TicketsComponent } from './tickets.component';
 
 describe('TicketsComponent', () => {
   let component: TicketsComponent;
   let fixture: ComponentFixture<TicketsComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TicketsComponent ]
+      declarations: [ TicketsComponent ],
+      providers: [
+        { provide: TicketsService, useClass: MockTicketsService }
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(TicketsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
