fdeee79f2b6d8c77888948d90b748d3709a34600
Shak623 <shak.farooq2@gmail.com>
2021-08-02 17:48:13 -0400

    Added mock data

---
+++ zendesk-ticket-app/src/app/models/CountResponseModel.mocks.ts
@@ -0,0 +1,6 @@
+import { CountResponseModel } from "./CountResponseModel";
+
+export const mockCountModel1: CountResponseModel = {
+    value: 0,
+    refreshed_at: new Date(2021, 1, 1)
+}
--- zendesk-ticket-app/src/app/ticket/ticket.component.spec.ts
+++ zendesk-ticket-app/src/app/ticket/ticket.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TicketComponent } from './ticket.component';
+import { mockTicketModel1 } from '../models/TicketResponseModel.mocks';
+import { mockCountModel1 } from '../models/CountResponseModel.mocks';
 
 describe('TicketComponent', () => {
   let component: TicketComponent;
